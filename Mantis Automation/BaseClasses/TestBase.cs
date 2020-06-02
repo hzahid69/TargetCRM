f26c6a3b7b846adc759b7a7fc9d35a9932c6f898
hzahid69 <35507276+hzahid69@users.noreply.github.com>
2020-06-02 13:34:10 +0500

    Changes

--- Mantis Automation/BaseClasses/TestBase.cs
+++ Mantis Automation/BaseClasses/TestBase.cs
@@ -5,21 +5,7 @@ using System.Configuration;
 
 namespace TargetCRM_Automation.BaseClasses
 {
-    /* public enum BrowserType
-     {
-         Chrome,
-         Firefox,
-         IE,
-         Opera
-     }
 
-     [TestFixture(BrowserType.Chrome)]
-     [TestFixture(BrowserType.Firefox)]
-     [TestFixture(BrowserType.Opera)]
-     [TestFixture(BrowserType.IE)]
-     [Parallelizable(ParallelScope.Fixtures)]*/
-
-    //[WatchDog(SaveInClass.AllTests)]
     public class TestBase
     {
 
@@ -35,11 +21,6 @@ namespace TargetCRM_Automation.BaseClasses
         public void TearDownTest()
         {
 
-            if (NUnit.Framework.TestContext.CurrentContext.Result.FailCount > 0)
-            {
-                string nomeArquivo = TakeScreenshotsHelper.TakeScreenshotOnException(driver, NUnit.Framework.TestContext.CurrentContext.Test.MethodName);
-                NUnit.Framework.TestContext.AddTestAttachment(System.AppDomain.CurrentDomain.BaseDirectory + "Screenshots/" + nomeArquivo, "evidência da tela onde o erro ocorreu!");
-            }
             driver.Quit();
         }
     }
